eae41d4e6c9db6bb551ab50495f3f1c8c1b4ec1b
Andres Eduardo Quezada Bendaña <andresequez@gmail.com>
2023-05-16 12:14:43 -0600

    Conditions, Footer and End

--- quesoApple/src/App.jsx
+++ quesoApple/src/App.jsx
@@ -5,6 +5,7 @@ import Card from './components/Card/Card'
 import MiniCard from './components/MiniCard/MiniCard'
 import miniCardData from './store/miniCardData'
 import cardData from './store/cardData'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
@@ -41,7 +42,43 @@ function App() {
       <div className="Carrousel">
         <img src="https://imgur.com/ZvgdDO8.png" alt="Carrousel" />
       </div>
-      
+      <div className="FooterA">
+        <div className="ConditionsText">
+          <p>
+            1. Trade-in values will vary based on the condition, year, and configuration of your eligible trade-in device.
+            Not all devices are eligible for credit. You must be at least 18 years old to be eligible to trade in for credit or for an Apple Gift Card. Trade-in value may be applied toward qualifying new device purchase, or added to an Apple Gift Card. Actual value awarded is based on receipt of a qualifying device matching the description provided when estimate was made. Sales tax may be assessed on full value of a new device purchase. In-store trade-in requires presentation of a valid photo ID (local law may require saving this information). Offer may not be available in all stores, and may vary between in-store and online trade-in. Some stores may have additional requirements. Apple or its trade-in partners reserve the right to refuse or limit quantity of any trade-in transaction for any reason. More details are available from Apple’s trade-in partner for trade-in and recycling of eligible devices. Restrictions and limitations may apply.
+            <br />
+            To access and use all the features of Apple Card, you must add Apple Card to Wallet on an iPhone or iPad with the latest version of iOS or iPadOS. Update to the latest version by going to Settings &gt; General &gt; Software Update. Tap Download and Install.
+            <br />
+            Available for qualifying applicants in the United States.
+            <br />
+            Apple Card is issued by Goldman Sachs Bank USA, Salt Lake City Branch.
+            <br />
+            Learn more about how Apple Card applications are evaluated at support.apple.com/kb/HT209218.
+            <br />
+            A subscription is required for Apple TV+.
+            <br />
+            Major League Baseball trademarks and copyrights are used with permission of MLB Advanced Media, L.P. All rights reserved.
+          </p>
+        </div>
+        <hr className="hr-divider" />
+        <div className="FooterLinks">
+          <Footer />
+        </div>
+        <div className="End">
+          <p className="FooterInfo1">More ways to shop: Find an Apple Store or other retailer near you. Or call 1-800-MY-APPLE.</p>
+          <hr className="hr-divider" />
+          <div className="FooterInfo2">
+            <li>Copyright © 2023 Apple Inc. All rights reserved.</li>
+            <li>Privacy Policy</li>
+            <li>Terms of Use</li>
+            <li>Sales and Refunds</li>
+            <li>Legal</li>
+            <li>Site Map</li>
+            <li>United States</li>
+          </div>
+        </div>
+      </div>
 
     </div>
   )
---
+++ quesoApple/src/components/Footer/Footer.jsx
@@ -0,0 +1,115 @@
+import './Footer.css'
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="FooterContainer">
+      <div>
+        <h1>Shop and Learn</h1>
+        <ul>
+          <li>Store</li>
+          <li>Mac</li>
+          <li>iPad</li>
+          <li>iPhone</li>
+          <li>Watch</li>
+          <li>AirPods</li>
+          <li>TV & Home</li>
+          <li>AirTag</li>
+          <li>Accessories</li>
+          <li>Gift Cards</li>
+        </ul>
+        <h1>Apple Wallet</h1>
+        <ul>
+          <li>Wallet</li>
+          <li>Apple Card</li>
+          <li>Apple Pay</li>
+          <li>Apple Cash</li>
+        </ul>
+      </div>
+      <div>
+        <h1>Account</h1>
+        <ul>
+          <li>Manage Your Apple ID</li>
+          <li>Apple Store Account</li>
+          <li>iCloud.com</li>
+        </ul>
+        <h1>Entertainment</h1>
+        <ul>
+          <li>Apple One</li>
+          <li>Apple TV+</li>
+          <li>Apple Music</li>
+          <li>Apple Arcade</li>
+          <li>Apple Fitness+</li>
+          <li>Apple News+</li>
+          <li>Apple Podcasts</li>
+          <li>Apple Books</li>
+          <li>App Store</li>
+        </ul>
+      </div>
+      <div>
+        <h1>Apple Store</h1>
+        <ul>
+          <li>Find a Store</li>
+          <li>Genius Bar</li>
+          <li>Today at Apple</li>
+          <li>Apple Camp</li>
+          <li>Apple Store App</li>
+          <li>Certified Refurbished</li>
+          <li>Apple Trade In</li>
+          <li>Financing</li>
+          <li>Carrier Deals at Apple</li>
+          <li>Order Status</li>
+          <li>Shopping Help</li>
+        </ul>
+      </div>
+      <div>
+        <h1>For Business</h1>
+        <ul>
+          <li>Apple and Business</li>
+          <li>Shop for Business</li>
+        </ul>
+        <h1>For Education</h1>
+        <ul>
+          <li>Apple and Education</li>
+          <li>Shop for K-12</li>
+          <li>Shop for College</li>
+        </ul>
+        <h1>For Healthcare</h1>
+        <ul>
+          <li>Apple in Healthcare</li>
+          <li>Health on Apple Watch</li>
+          <li>Health Records on iPhone</li>
+        </ul>
+        <h1>For Government</h1>
+        <ul>
+          <li>Shop for Government</li>
+          <li>Shop for Veterans and Military</li>
+        </ul>
+      </div>
+      <div>
+        <h1>Apple Values</h1>
+        <ul>
+          <li>Accessibility</li>
+          <li>Education</li>
+          <li>Environment</li>
+          <li>Inclusion and Diversity</li>
+          <li>Privacy</li>
+          <li>Racial Equity and Justice</li>
+          <li>Supplier Responsibility</li>
+        </ul>
+        <h1>About Apple</h1>
+        <ul>
+          <li>Newsroom</li>
+          <li>Apple Leadership</li>
+          <li>Career Opportunities</li>
+          <li>Investors</li>
+          <li>Ethics & Compliance</li>
+          <li>Events</li>
+          <li>Contact Apple</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
